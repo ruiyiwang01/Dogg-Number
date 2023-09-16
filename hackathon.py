0a2d82ddfe5dbef49293361418661cb4d1f10302
Ruiyi Wang <ruiwangk@mit.edu>
2023-09-16 14:31:31 -0400

    Finding all collaborators

--- hackathon.py
+++ hackathon.py
@@ -10,8 +10,17 @@ spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 snoopdogg_uri = "spotify:artist:7hJcb9fa4alzcOq3EaNPoG"
 
-results = spotify.search(q="artist:Snoop Dogg", limit=50, type="track")
-for i, t in enumerate(results["tracks"]["items"]):
-    if len(t["artists"]) > 1:
-        artists = [artist["name"] for artist in t["artists"]]
-        print(t["name"], artists)
+collaborators = set()
+for idx in range(0, 10000, 50):
+    try:
+        track_results = spotify.search(
+            q="artist:Snoop Dogg", limit=50, type="track", offset=idx
+        )
+    except:
+        break
+    for i, t in enumerate(track_results["tracks"]["items"]):
+        if len(t["artists"]) > 1:
+            for artist in t["artists"]:
+                if artist["name"] != "Snoop Dogg":
+                    collaborators.add(artist["name"])
+print(collaborators)
