fd66e30ba5df9f445dcabfe0159e655470fcaef2
Ruiyi Wang <ruiwangk@mit.edu>
2023-09-16 16:00:19 -0400

    Adding BFS logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from artist_graph import ArtistGraph
 
 artist_graph = ArtistGraph("Snoop Dogg")
-print(artist_graph.collaborators)
-print(artist_graph.collab_songs)
+collaborators = artist_graph.collaborators
+collabs = artist_graph.get_n_artists(2)
