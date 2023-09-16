a16120545bf2ec8d8ab46dbe805ae859ac4bf206
Ruiyi Wang <ruiwangk@mit.edu>
2023-09-16 17:10:01 -0400

    Removing credentials from main.py :

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from artist_graph import ArtistGraph
 import time
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-cid = "4b7803f7994c463e9b7143eeafc2d3ee"
-secret = "6eda60dc39b04d2c9c39e5c4beb58963"
-client_credentials_manager = SpotifyClientCredentials(
-    client_id=cid, client_secret=secret
-)
-spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 artist_graph = ArtistGraph("Taylor Swift")
 # test cases
