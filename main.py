b7bba22d23ea8fe32febba78b52d54e9b3ff9287
Ruiyi Wang <ruiwangk@mit.edu>
2023-09-16 17:07:13 -0400

    Optimizing BFS logic

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from artist_graph import ArtistGraph
+import time
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-artist_graph = ArtistGraph("Snoop Dogg")
-collaborators = artist_graph.collaborators
-collabs = artist_graph.get_n_artists(2)
+cid = "4b7803f7994c463e9b7143eeafc2d3ee"
+secret = "6eda60dc39b04d2c9c39e5c4beb58963"
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=cid, client_secret=secret
+)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+artist_graph = ArtistGraph("Taylor Swift")
+# test cases
+t1 = time.time()
+print(artist_graph.get_n_artists(1))
+t2 = time.time()
+print(f"Time taken: {t2-t1}")
+
+t1 = time.time()
+print(artist_graph.get_n_artists(2))
+t2 = time.time()
+print(f"Time taken: {t2-t1}")
+
+t1 = time.time()
+print(artist_graph.get_n_artists(1))
+t2 = time.time()
+print(f"Time taken: {t2-t1}")
