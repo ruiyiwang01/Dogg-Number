4c2c9a8b83f448e06187bafe1a193b3de26f1c77
Ruiyi Wang <ruiwangk@mit.edu>
2023-09-16 22:44:17 -0400

    Fix graph edges

--- vis.py
+++ vis.py
@@ -1,34 +1,16 @@
-import pyvis as pv
 from pyvis.network import Network
-import networkx as nx
 
-from artist_graph import ArtistGraph
 
 def create_graph(artist_data, n):
     pvNet = Network(height="100%", width="100%")
-    netxG = nx.Graph()
-    pvNet.add_node(artist_data.artist_name)
 
-    related_artists = artist_data.get_n_related_artists(n)
-    for artist_info in related_artists:
-        artist_name = artist_info  # No need to unpack, as it's a single value
-        pvNet.add_node(artist_name)
-        pvNet.add_edge(artist_data.artist_name, artist_name)
-
-    # print(pvNet.generate_html())
-    pvNet.from_nx(netxG)
+    artist_data.get_n_related_artists(n)
+    for layer_num in artist_data.cache:
+        for artist_name, _ in artist_data.cache[layer_num]:
+            pvNet.add_node(artist_name)
+            if artist_data.parents[artist_name]:
+                pvNet.add_edge(artist_name, artist_data.parents[artist_name])
 
     network_data = {"nodes": pvNet.nodes, "edges": pvNet.edges}
-
-    # pvNet.set_options(
-    #     physics=True,
-    #     layout={"improvedLayout": True},
-    #     edges={
-    #         "color": "gray",
-    #         "width": 1,
-    #     },
-    #     inherit_edge_colors=True,
-    #     toggle_hide_nodes_on_drag=True,
-    # )
 
     return network_data
