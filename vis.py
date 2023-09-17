dea2e055a4571dbd562f5f70953ec9c3d690289f
Ruiyi Wang <ruiwangk@mit.edu>
2023-09-16 21:50:16 -0400

    Fixing flask and pyvis integration

--- vis.py
+++ vis.py
@@ -4,21 +4,8 @@ import networkx as nx
 
 from artist_graph import ArtistGraph
 
-#  net = Network()
-#     net.add_node(related_artist_graph.artist_name)
-
-#     related_artists = related_artist_graph.get_n_related_artists(n)
-#     for artist_info in related_artists:
-#         artist_name = artist_info  # No need to unpack, as it's a single value
-#         net.add_node(artist_name)
-#         net.add_edge(related_artist_graph.artist_name, artist_name)
-
-
-#     net.save_graph(output_file)
 def create_graph(artist_data, n):
-    query_artist = artist_data.artist_name
-
-    pvNet = Network(height="800px", width="100%")
+    pvNet = Network(height="100%", width="100%")
     netxG = nx.Graph()
     pvNet.add_node(artist_data.artist_name)
 
